44e79e4809d9223cb0721afdb9699ece62e55ba1
Michal Smykowski <michal.smykowski03@gmail.com>
2025-12-17 00:21:21 +0100

    add api crud controller

---
+++ HotelApp/HotelApp/Controllers/HotelsApiController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelApp.Areas.Identity.Data;
+using HotelApp.Models;
+
+namespace HotelApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public HotelsApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/HotelsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
+        {
+            return await _context.Hotels.ToListAsync();
+        }
+
+        // GET: api/HotelsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hotel>> GetHotel(int id)
+        {
+            var hotel = await _context.Hotels.FindAsync(id);
+
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return hotel;
+        }
+
+        // PUT: api/HotelsApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotel(int id, Hotel hotel)
+        {
+            if (id != hotel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hotel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/HotelsApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+        {
+            _context.Hotels.Add(hotel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHotel", new { id = hotel.Id }, hotel);
+        }
+
+        // DELETE: api/HotelsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Hotels.Remove(hotel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HotelExists(int id)
+        {
+            return _context.Hotels.Any(e => e.Id == id);
+        }
+    }
+}
